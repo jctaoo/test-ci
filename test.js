51797a07488aab9f46d5d84203e38981646aeef4
jctaoo <jctaoo@outlook.com>
2021-06-11 10:14:45 +0000

    hello

--- test.js
+++ test.js
@@ -1,5 +1,13 @@
-function helloWorld() {
-    console.log("Hello World");
-}
+const express = require('express')
+const app = express()
+const port = 3000
 
-helloWorld();
+
+
+app.get('/api', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
